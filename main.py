1064088a6a4fa03c196a5e04254179442ca6195a
OctaviaL <l.bilmand@gmail.com>
2023-01-26 12:13:09 +0600

    new test

---
+++ main.py
@@ -0,0 +1,30 @@
+# file = open('text.txt', 'r')
+# file.write('Hello\n')
+# file.writelines('Ahahaha')
+# file.write('Pidorazz')
+# file.close()
+# file.write('Pidor')
+# f = file.read()
+# print(f)
+# print(file.readlines())
+# print(file.readlines())
+# file.close()
+
+# with open('text.txt') as file:
+#     x = 2
+#     print(file.read())
+
+# сериализация - dump, dumps (с пайтон в джейсон)
+# десерализация - load, loads (с джейсон в пайтон)
+
+# import json
+# d = {'hello': True, 'age': 2, 'name': None}
+# j_obj = json.dumps(d)
+# py_obj = json.loads(j_obj)
+# print(py_obj)
+
+# with open('data.json', 'r') as file:
+    # py_obj = json.loads(file.read())
+    # py_obj = json.load(file)
+    # print(py_obj)
+
